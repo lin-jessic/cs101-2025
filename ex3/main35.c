81ee13912529d194c3b940d9b5aaed34b81b180b
LIN, GUAN-YU <linguanyul@gmail.com>
2025-03-20 15:36:23 +0800

    Update main35.c

--- ex3/main35.c
+++ ex3/main35.c
@@ -6,9 +6,9 @@ int main() {
     p = &v;
     
     printf("Address of v: %x\n", v);
-    printf("Address stored in p:%x\n", p);
-    printf("Address of p:%x\n", &p);
-    printf("Value of *p:%d\n");
+    printf("Address stored in p: %x\n", p);
+    printf("Address of p: %x\n", &p);
+    printf("Value of *p: %d\n");
 
     return 0;
 }
