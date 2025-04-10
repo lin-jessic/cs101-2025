9fc0710d0940e5d50cd457dbfc1e29c33ac3b2af
LIN, GUAN-YU <linguanyul@gmail.com>
2025-04-10 15:40:07 +0800

    Update main26.c

--- ex4/main26.c
+++ ex4/main26.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-int multi(int i, int j) {
-    if (j == 1) {
-        printf("%d*%d=%d\t", i, j, i*j);
-        return 0;
-    }else {
-        multi(i, j-1);
-        printf("%d*%d=%d\t", i, j, i*j);
-    }
+int my_strlen(char* s) {
+    if (!*s) return 0;
+    return 1 + my_strlen(s+1);
 }
-void table (int i) {
-    if (i > 9) return;
-    else{ 
-        multi(i, 9);
-        table(i + 1);
-    }
-}
-int main() {
-    table(1);
+int main()
+{
+    char s[] = "IU is a girl!";
+    printf("(%s) len = %d\n", s, my_strlen(s));
     return 0;
 }
